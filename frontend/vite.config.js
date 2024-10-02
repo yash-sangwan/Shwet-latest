98d8708a89d0ef788704d6ccc36a9cde7ae16d4a
Aditi Bindal <123075271+aditi-dsi@users.noreply.github.com>
2024-10-02 17:21:50 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true, // Enable sourcemaps for easier debugging
+  },
   plugins: [
     react(),
   ],
